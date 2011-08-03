f79cb92e842a517ba79643bdc706428b8457f8df
unknown <Coxpit@.(none)>
2011-08-03 15:14:58 +0200

    Adding the movie-project in the state it is after following the tutorial. Planning to continue the work with this later on..

---
+++ Moovii/Moovii/Models/Movie.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations; 
+
+namespace Moovii.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Date is required")]
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required(ErrorMessage = "Genre must be specified")]
+        public string Genre { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        [Range(1, 100, ErrorMessage = "Price must be between $1 and $100")]
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public decimal Price { get; set; }
+        
+        [StringLength(5)]
+        public string Rating { get; set; }
+    }
+
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+    }
