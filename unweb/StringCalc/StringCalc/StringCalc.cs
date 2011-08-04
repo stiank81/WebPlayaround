b1f355cbaf99f5eff01f9faf3eb907b015ec6c23
unknown <Administrator@.(none)>
2011-08-04 15:51:07 +0200

    Adding StringCalc kata code - performed aug4th.

---
+++ unweb/StringCalc/StringCalc/StringCalc.cs
@@ -0,0 +1,67 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringCalc
+{
+    public class StringCalc
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+
+            var numberList = PrepareNumberList(numbers);
+ 
+            var sum = 0;
+            var negativeNumbers = new List<int>(); 
+            
+            foreach (var number in numberList)
+            {
+                var numAsInt = int.Parse(number);
+                if (numAsInt > 1000)
+                    continue;
+
+                if (numAsInt < 0)
+                    negativeNumbers.Add(numAsInt);
+                sum += numAsInt; 
+            }
+
+            if (negativeNumbers.Count > 0)
+                ThrowNegativeNumbersException(negativeNumbers); 
+
+            return sum; 
+        }
+
+        private string[] PrepareNumberList(string numbers)
+        {
+            var delimiters = GetDelimiters(numbers);
+
+            if (delimiters.Length > 2)
+                numbers = numbers.Substring(numbers.IndexOf('\n') + 1);
+
+            return numbers.Split(delimiters);
+        }         
+
+        private char[] GetDelimiters(string numbers)
+        {
+            if ((numbers.Length >= 3) && numbers.StartsWith("\\") && (numbers[2] == '\n'))
+                return new char[3] {',', '\n', numbers[1]};
+
+            return new char[2] {',', '\n'};
+        }
+
+        private void ThrowNegativeNumbersException(List<int> negativeNumbers)
+        {
+            var msg = "Negative numbers not allowed: ";
+            foreach (var num in negativeNumbers)
+            {
+                msg = msg + ", " + num; 
+            }
+            throw new ArgumentException(msg); 
+        }
+
+    }
+}
---
+++ unweb/StringCalc/StringCalc/Test/TestStringCalc.cs
@@ -0,0 +1,146 @@
+﻿
+using System;
+using NUnit.Framework; 
+
+namespace StringCalc.Test
+{
+    [TestFixture]
+    public class TestStringCalc
+    {
+        private StringCalc _calc;
+
+        [SetUp]
+        public void Setup()
+        {
+            _calc = new StringCalc(); 
+        }
+     
+        [Test]
+        public void Add_Empty_Zero()
+        {
+            Assert.AreEqual(0, _calc.Add(string.Empty));
+        }
+        
+        [Test]
+        public void Add_OneNumber_NumberAsInt()
+        {
+            const int number = 7;
+            var numAsString = number.ToString(); 
+            Assert.AreEqual(number, _calc.Add(numAsString));
+        }
+
+        [Test]
+        public void Add_TwoNumbers_Sum()
+        {
+            const int num1 = 7;
+            const int num2 = 11;
+            var numString = num1 + "," + num2; 
+            Assert.AreEqual(num1 + num2, _calc.Add(numString));
+        }
+        
+        [Test]
+        public void Add_ThreeNumbers_Sum()
+        {
+            const int num1 = 7;
+            const int num2 = 11;
+            const int num3 = 17;
+            var numString = num1 + "," + num2 + "," + num3;  
+            Assert.AreEqual(num1 + num2 + num3, _calc.Add(numString));
+        }
+
+        [Test]
+        public void Add_OnlyNewlineAsSeparator_Sum()
+        {
+            const int num1 = 1;
+            const int num2 = 2;
+            const int num3 = 3;
+            var numString = num1 + "\n" + num2 + "\n" + num3; 
+            Assert.AreEqual(num1 + num2 + num3, _calc.Add(numString));
+        }
+        
+        [Test]
+        public void Add_NewlineAndCommaSeparator_Sum()
+        {
+            const int num1 = 1;
+            const int num2 = 2;
+            const int num3 = 3;
+            var numString = num1 + "\n" + num2 + "," + num3; 
+            Assert.AreEqual(num1 + num2 + num3, _calc.Add(numString));
+        }
+
+        [Test]
+        public void Add_UsingCustomDelimiter_Sum()
+        {
+            const int num1 = 1;
+            const int num2 = 2;
+            const int num3 = 3;
+            var numString = "\\;\n" + num1 + ";" + num2 + ";" + num3;
+            Assert.AreEqual(num1 + num2 + num3, _calc.Add(numString));
+        }
+
+        [Test]
+        public void Add_OneNegativeNumber_ArgumentExceptionThrown()
+        {
+            const int num1 = 1;
+            const int num2 = -2;
+            var numString = num1 + "," + num2;
+            Assert.Throws<ArgumentException>(() => _calc.Add(numString)); 
+        }
+
+        [Test]
+        public void Add_OneNegativeNumber_ExceptionContainsNumber()
+        {
+            const int num1 = 1;
+            const int num2 = -2;
+            var numString = num1 + "," + num2;
+
+            try
+            {
+                _calc.Add(numString);
+            }
+            catch (ArgumentException e)
+            {
+                Assert.That(e.Message.Contains(num2.ToString()));
+            }
+        }
+        
+        [Test]
+        public void Add_MultipleNegativeNumbers_ExceptionContainsAllNegativeNumbers()
+        {
+            const int num1 = -1;
+            const int num2 = -2;
+            const int num3 = -3;
+            var numString = num1 + "," + num2 + "," + num3;
+
+            try
+            {
+                _calc.Add(numString); 
+            }
+            catch (ArgumentException e)
+            {
+                Assert.That(e.Message.Contains(num1.ToString()));
+                Assert.That(e.Message.Contains(num2.ToString()));
+                Assert.That(e.Message.Contains(num3.ToString()));
+            }
+        }
+
+        [Test]
+        public void Add_IncludingNumber1000_Sum()
+        {
+            const int num1 = 17;
+            const int num2 = 1000;
+            var numString = num1 + "," + num2; 
+            Assert.AreEqual(num1 + num2, _calc.Add(numString));
+        }
+        
+        [Test]
+        public void Add_IncludingNumberGreaterThan1000_SumWithoutTooBigNumber()
+        {
+            const int num1 = 17;
+            const int num2 = 1000;
+            const int num3 = 1001;
+            var numString = num1 + "," + num2 + "," + num3; 
+            Assert.AreEqual(num1 + num2, _calc.Add(numString));
+        }            
+    }
+}
